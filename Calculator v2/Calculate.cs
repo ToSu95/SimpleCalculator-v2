63f14f5b13032538c1ded588e04ace6b9d4c0085
ToSu95 <t.suska95@gmail.com>
2022-07-23 15:56:52 +0200

    Add project files.

---
+++ Calculator v2/Calculate.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_v2
+{
+    internal class Calculate
+    {
+        public int GetNumberOne()
+        {                                         
+
+            if (!int.TryParse(Console.ReadLine(), out int number))
+            {
+              Console.WriteLine("Podałeś nieprawidłowe dane!");
+                
+            }
+
+            return number;
+            
+        }
+
+        public int GetNumberTwo()
+        {
+
+            if (!int.TryParse(Console.ReadLine(), out int number))
+            {
+              Console.WriteLine("Podałeś nieprawidłowe dane!");
+
+            }
+
+            return number;
+
+        }        
+
+        public int GetAction(int numberOne, int numberTwo, string action)
+        {
+            switch(action)
+            {
+                case "+":
+                    return numberOne + numberTwo;   
+                case "-":
+                    return numberOne - numberTwo;
+                case "*":
+                    return numberOne * numberTwo;
+                case "/":
+                    return numberOne / numberTwo;
+                default:
+                    throw new Exception("Wybrałeś złą operację!");
+            }
+        }
+    }
+}
---
+++ Calculator v2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var calculator = new Calculate();
+
+            while (true)
+            {
+            Console.WriteLine("Prosty kalkulator");
+            
+
+            Console.WriteLine("Podaj 1 liczbę:");
+            var numberOne = calculator.GetNumberOne();
+
+            Console.WriteLine("Podaj 2 liczbę:");
+            var numberTwo = calculator.GetNumberTwo();
+
+            Console.WriteLine("Podaj operację jaką chcesz wykonać, możliwe operacje to: '+', '-', '*', '/'.");
+            var action = Console.ReadLine();
+
+            var result = calculator.GetAction(numberOne, numberTwo, action);
+
+            Console.WriteLine("Wynik: {0}", result);
+
+            Console.WriteLine("Jeśli chcesz wyjść z kalkulatora, podaj 'n'. W przeciwnym wypadku podaj 'y'");
+
+            var userChoice = Console.ReadLine();
+
+            if (userChoice == "n")
+              {
+                Environment.Exit(0);
+              }
+            else if (userChoice == "y")
+              {
+                continue;
+              }
+
+            }
+        }
+    }
+}
